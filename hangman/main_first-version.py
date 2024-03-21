744f4c8ddbb57b2690b8f61be688f6bc36b01e13
RajAdarsh2022 <inforaj.2022@gmail.com>
2024-03-21 21:45:44 +0530

    improved UI

--- hangman/main_first-version.py
+++ hangman/main_first-version.py
@@ -1,12 +1,13 @@
+from hangman_art import logo, stages
+from hangman_words import word_list
 import random
 
+
 # Initializing the game and providing instructions
-print("Welcome to Hangman !")
-print("You have 7 lives in total, so guess correctly")
+print(logo)
 print("All the Best !")
 print("*******************************************************")
 
-word_list = ["aardvark", "baboon", "camel"]
 life_count = 7
 isTerminate = False
 
@@ -15,16 +16,17 @@ chosen_word = random.choice(word_list)
 chosen_word_length = len(chosen_word)
 word_till_now = ['_'] * chosen_word_length
 
+
 while not isTerminate:
     # Asking the user to guess a letter and making guess lowercase.
-    guess = input("Enter your guess: ")
-    letter = guess.lower()
+    guess = input("Enter your guess: ").lower()
+
 
     # Checking if the letter the user guessed (guess) is one of the letters in the chosen_word.
-    if letter in chosen_word:
+    if guess in chosen_word:
         for i in range(len(chosen_word)):
-            if chosen_word[i] == letter:
-                word_till_now[i] = letter
+            if chosen_word[i] == guess:
+                word_till_now[i] = guess
         if ''.join(word_till_now) == chosen_word:
             break
         else:
@@ -33,6 +35,7 @@ while not isTerminate:
     else:
         # One life goes away
         life_count -= 1
+        print(stages[life_count])
         print(f"Oops, You guessed incorrectly! You have {life_count} chances remaining.")
 
         if life_count == 0:
@@ -42,7 +45,7 @@ if isTerminate:
     print("Sorry you lost!")
 else:
     print("Congratulations! You Won !!!")
-    print(f"The word was {chosen_word}")
+print(f"The word was {chosen_word}")
 
 
     
