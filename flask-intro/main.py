80d32d1b0ac7da4db5089164f764dbff84926aa0
RajAdarsh2022 <inforaj.2022@gmail.com>
2024-07-07 23:25:43 +0530

    understanding template rendering and static files-1

--- flask-intro/main.py
+++ flask-intro/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import random
 
 app = Flask(__name__)
@@ -6,31 +7,10 @@ random_number = random.randint(0,9)
 
 
 @app.route("/")
-def guess_number():
-    return """
-            <h1>Guess a number between 0 and 9</h1>
-            <img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>
-        """
+def home():
+    return render_template('index.html')
 
 
-@app.route("/<int:user_guess>")
-def check_answer(user_guess):
-    
-    if user_guess < random_number:
-        return """
-                <h1 style="color:red;">Too Low, Try Again! </h1>
-                <img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>
-            """
-    elif user_guess > random_number:
-        return """
-                <h1 style="color:purple;">Too high, Try Again!</h1>
-                <img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>
-            """
-    else:
-        return """
-                <h1 style="color:green;">You found me!</h1>
-                <img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>
-            """
 
 if __name__ == "__main__":
     app.run()
